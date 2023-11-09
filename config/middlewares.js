80fbcc10f5ac0955a8a4a50fdfd808000ccb5a38
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2023-11-10 01:00:41 +0530

    corsfix

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,8 +14,15 @@ module.exports = [
       },
     },
   },
-  'strapi::cors',
   'strapi::poweredBy',
+{
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:1337', 'https://strapimaster-production.up.railway.app']
+    }
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
